06f7abce29fd5c07e5825094bdfe3c61fb878580
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-22 12:40:30 -0600

    feat(Search for user by key value pair):

--- repositories/users.js
+++ repositories/users.js
@@ -64,6 +64,22 @@ class UsersRepository {
         Object.assign(record, attrs);
         await this.writeAll(records);
     }
+
+    async getOneBy(filters) {
+        const records = await this.getAll();
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            if (found) {
+                return record;
+            }
+        }
+    }
 }
 
 const test = async () => {
