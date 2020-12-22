4c193e8fc12d5045ce8bf0078d46567275261298
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-22 11:06:27 -0600

    refactor: Code Readability
    Made code easier to read.

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send(`
-        <div>
-            <form method='POST'>
-                <input name='email' placeholder='email'/>
-                <input name='password' placeholder='password'/>
-                <input name='passwordconfirmation' placeholder='password confirmation'/>
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
 });
-
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.send('Account created!')
+    res.send('Account created!!!');
 });
 
 app.listen(3000, () => {
-    console.log('Listening')
+    console.log('Listening');
 });
--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs');
 
 class UsersRepository {
     constructor(filename) {
@@ -7,24 +7,37 @@ class UsersRepository {
         }
 
         this.filename = filename;
-        try{
+        try {
             fs.accessSync(this.filename);
         } catch (err) {
-          fs.writeFileSync(this.filename, '[]');
+            fs.writeFileSync(this.filename, '[]');
         }
     }
+
     async getAll() {
-        const contents = JSON.parse(
+        return JSON.parse(
             await fs.promises.readFile(this.filename, {
                 encoding: 'utf8'
             })
         );
     }
+
+    async create(attrs) {
+        const records = await this.getAll();
+        records.push(attrs);
+
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
+
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
+    await repo.create({ email: 'test@test.com', password: 'password' });
+
     const users = await repo.getAll();
-}
+
+    console.log(users);
+};
 
 test();
