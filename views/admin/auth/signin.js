dda474f65834e0eec01202538a4fb86d6b6544a6
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-27 17:41:11 -0600

    refactor: Created HTML layout.js for signin.js and signup.js HTML.

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,12 +1,16 @@
+const layout = require('../layout');
+
 module.exports = () => {
-    return `
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign In</button>
-      </form>
-  </div>
-    `;
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input name="email" placeholder="email" />
+          <input name="password" placeholder="password" />
+          <input name="passwordConfirmation" placeholder="password confirmation" />
+          <button>Sign In</button>
+        </form>
+      </div>
+    `
+  });
 }
