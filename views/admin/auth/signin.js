3087c7e921e14b887d5dbf7921c79809a14ddf8b
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-27 14:40:13 -0600

    refactor: Put signin and signup HTML in separate JS files.

---
+++ views/admin/auth/signin.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+    return `
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign In</button>
+      </form>
+  </div>
+    `;
+}
