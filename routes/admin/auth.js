6e281807c311e957e42812293e8f08682ecb78a6
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-28 08:59:04 -0600

    refactor: Relocated signin validators.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,9 +4,12 @@ const { check, validationResult } = require('express-validator')
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
-const { requireEmail } = require('./validators');
-const { requirePassword } = require('./validators');
-const { requirePasswordConfirmation } = require('./validators');
+const { requireEmail,
+        requirePassword,
+        requirePasswordConfirmation,
+        requireEmailExists,
+        requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
@@ -40,24 +43,16 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await usersRepo.getOneBy({ email: email });
+router.post('/signin',
+    [requireEmailExists, requireValidPasswordForUser],
+    async (req, res) => {
+        const errors = validationResult(req);
+        const { email } = req.body;
+        const user = await usersRepo.getOneBy({ email: email });
 
-    if (!user) {
-        return res.send('Email not found');
-    }
-    const validPassword = await usersRepo.comparePasswords(
-        user.password,
-        password
-    )
+        req.session.userID = user.id;
 
-    if (!validPassword) {
-        return res.send('Invalid password');
-    }
-    req.session.userID = user.id;
-
-    res.send('You are signed in!!!');
+        res.send('You are signed in!!!');
 })
 
 module.exports = router;
