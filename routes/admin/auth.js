e052b1a89e689c34d56e017c1152234522401588
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-27 18:49:20 -0600

    feat: Added custom validation code and messages.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -11,33 +11,40 @@ router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req: req }));
 });
 
-router.post('/signup', [
-    check('email').trim().normalizeEmail().isEmail(),
-    check('password').trim().isLength({ min: 4, max: 20 }),
-    check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+router.post('/signup',
+ [
+    check('email')
+        .trim()
+        .normalizeEmail()
+        .isEmail()
+        .withMessage('Must be between 4 and 20 characters')
+        .custom(async (email) => {
+            const existingUser = await usersRepo.getOneBy({ email: email });
+            if (existingUser) {
+                throw new Error('Email in use');
+            }
+        }),
+    check('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Must be between 4 and 20 characters'),
+    check('passwordConfirmation')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Must be between 4 and 20 characters')
+        .custom((passwordConfirmation, { req }) => {
+            if (password !== req.body.password) {
+                throw new Error('Passwords must match');
+            }
+        })
 ],
 async (req, res) => {
     const errors = validationResult(req);
-
     const { email, password, passwordConfirmation } = req.body;
 
-    const existingUser = await usersRepo.getOneBy({ email: email });
-    if (existingUser) {
-        return res.send('Email in use')
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match');
-    }
+    req.session.userID = user.id;
 
     res.send('Account created!!!');
-
-    // Create a user in our user repo to represent this person
-    const user = await usersRepo.create({ email: email, password: password });
-
-    // Store the id of that user inside the users cookie
-    req.session.userID === user.id; //Added by cookie session.
-
 });
 
 router.get('/signout', (req, res) => {
