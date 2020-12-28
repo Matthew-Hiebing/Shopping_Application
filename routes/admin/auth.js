b4da6bbb8340e89d936eaabe5cbe7a306108f09a
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-28 08:03:19 -0600

    refactor: Relocated validators to separate files to make auth file more readable.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,6 +4,9 @@ const { check, validationResult } = require('express-validator')
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
+const { requireEmail } = require('./validators');
+const { requirePassword } = require('./validators');
+const { requirePasswordConfirmation } = require('./validators');
 
 const router = express.Router();
 
@@ -12,39 +15,14 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup',
- [
-    check('email')
-        .trim()
-        .normalizeEmail()
-        .isEmail()
-        .withMessage('Must be between 4 and 20 characters')
-        .custom(async (email) => {
-            const existingUser = await usersRepo.getOneBy({ email: email });
-            if (existingUser) {
-                throw new Error('Email in use');
-            }
-        }),
-    check('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Must be between 4 and 20 characters'),
-    check('passwordConfirmation')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Must be between 4 and 20 characters')
-        .custom((passwordConfirmation, { req }) => {
-            if (password !== req.body.password) {
-                throw new Error('Passwords must match');
-            }
-        })
-],
+ [requireEmail, requirePassword, requirePasswordConfirmation],
 async (req, res) => {
     const errors = validationResult(req);
     const { email, password, passwordConfirmation } = req.body;
 
     req.session.userID = user.id;
 
-    res.send('Account created!!!');
+    res.send('Account created!');
 });
 
 router.get('/signout', (req, res) => {
