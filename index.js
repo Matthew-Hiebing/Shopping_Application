7167f2a05e6622bfc263b815864bb42c666921ec
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-28 16:00:19 -0600

    refactor: Re-organized index.js.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session')
-const authRouter = require('./routes/admin/auth')
-const productsRouter = require('./routes/admin/products')
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({
-  keys: ['dsfdsafdasfdf35kd32']
-}))
-
-app.listen(3000, () => {
-    console.log('Listening');
-});
+app.use(
+  cookieSession({
+    keys: ['lkasld235j']
+  })
+);
 app.use(authRouter);
 app.use(productsRouter);
+
+app.listen(3000, () => {
+  console.log('Listening');
+});
