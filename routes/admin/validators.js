6dedcf8c61a5c755b98a9ae69e7d60fd5b11bf72
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-29 08:30:56 -0600

    feat: Added title and price message validators on new product creation page.

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -4,11 +4,13 @@ const usersRepo = require('../../repositories/users');
 module.exports = {
     requireTitle: check('title')
         .trim()
-        .isLength({ min: 5, max: 40 }),
-    requirePrice: check('price')
+        .isLength({ min: 5, max: 40 })
+        .withMessage('Must be between 5 and 40 characters'),
+        requirePrice: check('price')
         .trim()
         .toFloat()
-        .isFloat({ min: 1 }),
+        .isFloat({ min: 1 })
+        .withMessage('Must be a number greater than one.'),
     requireEmail: check('email')
         .trim()
         .normalizeEmail()
