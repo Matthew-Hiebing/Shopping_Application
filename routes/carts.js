209846a53183bc4b25f1d6b02f55ff7bad5ae918
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-12-29 18:46:51 -0600

    feat: Added route for cart items and created a hidden product id value that is linked to an item when it is added to the cart.

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
+
+router.post('/cart/products', (req, res) => {
+let cart;
+if (!req.session.cartId) {
+    const cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cartId;
+} else {
+    cart = await cartsRepo.getOne(req.session.cartId);
+}
+
+    res.send('Product added to cart');
+})
 
 module.exports = router;
 
